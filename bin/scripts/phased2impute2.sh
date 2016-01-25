8596c8a2d6c551a42ab2166c243d82804b776494
Sahar <smozaffari@uchicago.edu>
2016-01-25 16:00:53 -0600

    Update phased2impute2.sh

--- bin/scripts/phased2impute2.sh
+++ bin/scripts/phased2impute2.sh
@@ -19,9 +19,10 @@ module load plink
 plink --bfile /group/ober-resources/resources/Hutterites/PRIMAL/imputed-override3/imputed_cgi.po --keep PO_ids --missing --out phasedPO_AD_gexppl --recode --transpose
 
 #separate by chromosome
-for snp in `seq 1 22` 
+for chr in `seq 1 22` 
 do
-  awk -v snp=$snp -F" " '$1 == "'"$snp"'" {print}' phasedPO_AD_gexppl.tped | awk '{for(i=2;i<=NF;i=i+2){printf "%s ", $i}{printf "%s", RS}}' > 'chr'$snp'_phased'
+  awk -v chr=$chr -F" " '$1 == "'"$chr"'" {print}' phasedPO_AD_gexppl.tped | awk '{for(i=2;i<=NF;i=i+2){printf "%s ", $i}{printf "%s", RS}}' > 'chr'$chr'_phased'
+  gzip 'chr'$chr'_phased'
 done
 
 #translate to impute2 haplotype format
@@ -29,8 +30,13 @@ perl phased2imputehaps.pl
 
 
 ##regular impute data
-#plink --bfile /group/ober-resources/resources/Hutterites/PRIMAL/data-sets/qc/qc --out allnotPOgtype --recode 12 --transpose
+#plink --bfile /group/ober-resources/resources/Hutterites/PRIMAL/data-sets/qc/qc --keep ../431_Hutt_gexp_ids --out allnotPOgtype --recode 12 --transpose
 
+for chr in `seq 1 22`
+do
+  awk -v chr=$chr -F" " '$1 == "'"$chr"'" {print}' allnotPOgtype.tped > 'chr'$chr'_gtype'
+  gzip 'chr'$chr'_gtype'
+done
 
 
 
