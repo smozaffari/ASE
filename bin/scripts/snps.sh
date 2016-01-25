ea11871d399dea4670ef14b26684efc07b43f45c
Sahar <smozaffari@uchicago.edu>
2016-01-25 11:29:47 -0600

    Update snps.sh

--- bin/scripts/snps.sh
+++ bin/scripts/snps.sh
@@ -1,5 +1,4 @@
-#/bin/bash
-
+#!/bin/bash
 
 for i in seq `1..2` {
   awk -v snp=$snp -F" " '$1 == $'$snp' {print}' phasedPO_g0.15_AD_gexppl.tped | awk '{for(i=2;i<=NF;i=i+2){printf "%s ", $i}{printf "%s", RS}}' > 'chr'$snp'_phased'
