8b68aed88635518f9f4eecc3e2dd78ee7c594977
Sahar <smozaffari@uchicago.edu>
2016-01-25 13:54:19 -0600

    Update phased2impute2.sh

--- bin/scripts/phased2impute2.sh
+++ bin/scripts/phased2impute2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PBS -N phase_ped
+#PBS -N ph2imp2
 #PBS -l walltime=2:00:00:00
 #PBS -l nodes=1:ppn=1
-#PBS -l mem=15gb
-#PBS -e phase_ped.err
-#PBS -o phase_ped.log
+#PBS -l mem=4gb
+#PBS -e ph2imp2.err
+#PBS -o ph2imp2.log
 #PBS -M smozaffari@uchicago.edu
 
 export TMPDIR=$WORKDIR
@@ -13,17 +13,20 @@ export TEMP=$WORKDIR
 
 module load plink
 
-#PO data
+##PO data
+
+#get PO genotypes
 plink --bfile /group/ober-resources/resources/Hutterites/PRIMAL/imputed-override3/imputed_cgi.po --keep PO_ids --missing --out phasedPO_AD_gexppl --recode 12 --transpose
 
-#regular data
-#from qc set
-
+#separate by chromosome
 for snp in `seq 1 22` 
 do
   awk -v snp=$snp -F" " '$1 == "'"$snp"'" {print}' phasedPO_AD_gexppl.tped | awk '{for(i=2;i<=NF;i=i+2){printf "%s ", $i}{printf "%s", RS}}' > 'chr'$snp'_phased'
 done
 
+#translate to impute2 haplotype format
 perl phased2imputehaps.pl
+
+
 
 
