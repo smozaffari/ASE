b596ce959ddce492818434a7c0d480edde645b9d
Sahar <smozaffari@uchicago.edu>
2016-01-26 16:40:41 -0600

    Update phased2impute2.sh

--- bin/scripts/phased2impute2.sh
+++ bin/scripts/phased2impute2.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#PBS -N ph2imp2
-#PBS -l walltime=2:00:00:00
-#PBS -l nodes=1:ppn=1
-#PBS -l mem=4gb
-#PBS -e ph2imp2.err
-#PBS -o ph2imp2.log
-#PBS -M smozaffari@uchicago.edu
 
 export TMPDIR=$WORKDIR
 cd $PBS_O_WORKDIR
