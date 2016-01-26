e55ab1dfad569b30d3615d0be7966913ec9f2b6d
Sahar <smozaffari@uchicago.edu>
2016-01-26 16:39:27 -0600

    Update phased2impute.pl

--- bin/scripts/phased2impute.pl
+++ bin/scripts/phased2impute.pl
@@ -3,6 +3,10 @@
 use strict;
 use warnings;
 
+my %ref;
+my %alt;
+my %rs;
+
 open (COD, "/group/ober-resources/users/smozaffari/POeQTL/data/ASE/recodealt.txt") || die "nope: $!";
 while (my $line = <COD>) {
     my @line = split " ", $line;
@@ -12,29 +16,29 @@ while (my $line = <COD>) {
     $rs{$snp} = $line[3];
 }
 
-for (my $i=1; $i<=22; $i++) {
+for (my $i=1; $i<=2; $i++) {
     my $chr = join "", "chr", $i, "_gtype";
-	open (RAW, "/group/ober-resources/users/smozaffari/POeQTL/data/ASE/$chr") || die "nope: $!";
-	 my $out = join "", "chr", $i, "_impute_gtype";
-	 open (IMP, ">$out") || die "nope: $!";
-	 my $line1 = <RAW>;
-	 while (my $line = <RAW>) {
-	 	my @line = split " ", $line;
-		my $length = $#line;
-		my $snp = $line[0];
-		my $loc = $line[1];
-		if ($ref{$snp}) {
-		    print IMP ("$snp $rs{$snp} $snp $loc $ref{$snp} $alt{$snp} ");
-		}
-	    foreach my $snp (6 .. $#line) {
-		if ($snp = 2) {
-		    print IMP ("1 0 0 ");
-		} elsif ($snp = 1) {
-	    		print IMP ("0 1 0 ");
-		} elsif ($snp = 0) {
-		    print IMP ("0 0 1");
-		}
-	    }
-	    print IMP ("\n");
+    open (RAW, "/group/ober-resources/users/smozaffari/POeQTL/data/ASE/$chr") || die "nope: $!";
+    my $out = join "", "chr", $i, "_impute_gtype";
+    open (IMP, ">$out") || die "nope: $!";
+    my $line1 = <RAW>;
+    while (my $line = <RAW>) {
+	my @line = split " ", $line;
+	my $length = $#line;
+	my $snp = $line[0];
+	my $loc = $line[1];
+	if ($ref{$snp}) {
+	    print IMP ("$snp $rs{$snp} $snp $loc $ref{$snp} $alt{$snp} ");
 	}
+	foreach my $snp (6 .. $#line) {
+	    if ($snp == 2) {
+		print IMP ("1 0 0 ");
+	    } elsif ($snp == 1) {
+		print IMP ("0 1 0 ");
+	    } elsif ($snp == 0) {
+		print IMP ("0 0 1");
+	    }
+	}
+	print IMP ("\n");
+    }
 }
