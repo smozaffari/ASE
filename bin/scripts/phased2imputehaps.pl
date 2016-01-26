c57ce7ac5a7f2d2a929e09ec8aa452e799d92d43
Sahar <smozaffari@uchicago.edu>
2016-01-26 16:38:40 -0600

    Update phased2imputehaps.pl

--- bin/scripts/phased2imputehaps.pl
+++ bin/scripts/phased2imputehaps.pl
@@ -16,19 +16,19 @@ while (my $line = <COD>) {
     $rs{$snp} = $line[3];
 }
 
-for (my $i=1; $i<=22; $i++) {
+for (my $i=1; $i<=2; $i++) {
     my $chr = join "", "chr", $i, "_phased";
-    open (APED, "/group/ober-resources/users/smozaffari/POeQTL/data/ASE/$chr") || die "nope: $!";
+    open (APED, "/group/ober-resources/users/smozaffari/POeQTL/data/ASE/phased_2_impute/$chr") || die "nope: $!";
     my $out = join "", "chr", $i, "_imputehaps";
     open (OUT, ">$out") || die "nope: $!";
-    my $line1 = <APED>;
+#    my $line1 = <APED>;
     while (my $line = <APED>) {
 	my @line = split " ", $line;
 	my $length = $#line;
 	my $snp = $line[0];
 	my $loc = $line[1];
 	if ($ref{$snp}) {
-	    print OUT ("--- $rs{$snp} $snp $loc $ref{$snp} $alt{$snp} ");
+	    print OUT ("$snp $rs{$snp} $snp $loc $ref{$snp} $alt{$snp} ");
 	    for (my $j=2; $j <= $length; $j++) {
 		if ($line[$j]) {
 		    if ($line[$j] eq $ref{$snp}) {
@@ -39,7 +39,7 @@ for (my $i=1; $i<=22; $i++) {
 		} else {
 		    print OUT "0 ";
 		}
-	    }	
+	    }
 	    print OUT "\n";
 	}
     }
