ba97645ce1623b1420be3ea581efbacd38212b4c
Sahar <smozaffari@uchicago.edu>
2016-02-15 17:09:33 -0600

    Create po_genotypes.sh

---
+++ bin/tarbellscripts/po_genotypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+# to submit this, enter './adaptor_list_through.sh' 
+# the read command is going to look at whatever file you pipe into the script with the < operator
+while read LINE; do
+	echo "$LINE"
+  cat "HUTTERITES ${LINE}2" > ${LINE}2.txt
+  cat "HUTTERITES ${LINE}1" > ${LINE}1.txt
+	echo "qsub -v FINDIV=$LINE geno.pbs"
+	# qsub -v FINDIV=$LINE geno.pbs
+done < /group/ober-resources/users/smozaffari/findivlist
