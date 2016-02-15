21f88ae248e9c1f704dc90acc405418be1fc7b6f
smozaffari <smozaffari@uchicago.edu>
2016-02-15 17:27:16 -0600

    genotype files for po

--- bin/tarbellscripts/po_genotypes.sh
+++ bin/tarbellscripts/po_genotypes.sh
@@ -1,11 +1,17 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 # to submit this, enter './adaptor_list_through.sh' 
 # the read command is going to look at whatever file you pipe into the script with the < operator
+scriptName=$(basename ${0})
+scriptName=${scriptName%\.sh}
+scriptDir=$(readlink -f "$(dirname "$0")")
+echo $scriptDir
+
+
 while read LINE; do
-	echo "$LINE"
-  cat "HUTTERITES ${LINE}2" > ${LINE}2.txt
-  cat "HUTTERITES ${LINE}1" > ${LINE}1.txt
-	echo "qsub -v FINDIV=$LINE geno.pbs"
-	# qsub -v FINDIV=$LINE geno.pbs
-done < /group/ober-resources/users/smozaffari/ASE/data/findivlist
+    echo "$LINE"
+    echo "HUTTERITES ${LINE}2" > ${LINE}2.txt
+    echo "HUTTERITES ${LINE}1" > ${LINE}1.txt
+    echo "qsub -v FINDIV=$LINE $scriptDir/geno.pbs"
+    qsub -v FINDIV=$LINE $scriptDir/geno.pbs
+done < /group/ober-resources/users/smozaffari/ASE/data/testfindivlist
