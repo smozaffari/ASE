82274089d5ac613750e3a6798cf55c474ea91ab8
Sahar Mozaffari <smozaffari@uchicago.edu>
2016-04-01 14:11:01 -0500

    only run 1

--- bin/scripts/newmaster.sh
+++ bin/scripts/newmaster.sh
@@ -91,9 +91,9 @@ for dir in $inputDirs;do
 		fi
 	    done
 	fi
-#	if [[ "$count" == 5 ]]; then
-#	    exit
-#	fi
+	if [[ "$count" == 1 ]]; then
+	    exit
+	fi
 	    
     fi
 done | tee -a $setup_log
