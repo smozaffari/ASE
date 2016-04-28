8ad1b0244b6627a9f21354bdb5d7dcfe790dada1
smozaffari <smozaffari@uchicago.edu>
2016-04-28 13:56:02 -0500

    updates from a whle ago...

--- bin/tarbellscripts/genotypes_reg.sh
+++ bin/tarbellscripts/genotypes_reg.sh
@@ -12,6 +12,7 @@ while read LINE; do
     echo $LINE
     echo "HUTTERITES ${LINE}" > $LINE/${LINE}.txt
     echo "qsub -v FINDIV=$LINE $scriptDir/geno_reg.pbs"
-    qsub -v FINDIV=$LINE $scriptDir/geno_reg.pbs
+#    qsub -v FINDIV=$LINE $scriptDir/geno_reg.pbs
+    qsub -v FINDIV=$LINE /group/ober-resources/users/smozaffari/ASE/bin/tarbellscripts/geno.pbs
     sleep 3
-done < /group/ober-resources/users/smozaffari/ASE/data/findivlist
+done < /group/ober-resources/users/smozaffari/ASE/data/newfull_findivlist
