cbfe10c2082df0b1e991d577b499b40b4d715564
Sahar Mozaffari <smozaffari@uchicago.edu>
2016-05-02 10:10:39 -0500

    annotation

--- bin/scripts/gene.pl
+++ bin/scripts/gene.pl
@@ -18,13 +18,26 @@ while (my $line = <ANN>) {
     $ann{$chr}{$loc} = $line[5];
 }
 
-
+open (FILES, "/lustre/beagle2/ober/users/smozaffari/ASE/results/test_ASE") || die "nope: $!";
+while (my $fileline = <FILES>) {
+    my @findiv_lane = split "/", $fileline;
+    my $name = $findiv_lane[3];
+    chomp $name;
+    my $outfile = "out_$name.txt";
+    open (OUT, ">summarystats/$outfile") || die "nope: $!";
+    open (ASE, $fileline) || die "nope: $!";
 #open (ASE, "/lustre/beagle2/ober/users/smozafari/ASE/results/withoutsaved/FlowCell8/122462/122462_lane_5_ASE_info") || die "nope: $!";
-open (ASE, "test_5") || die "nope: $!";                                                                                                        
-while (my $line = <ASE>) {
-    my @line = split "\t", $line;
-    my $chr = $line[0];
-    my $snp = $line[1];
-    print ( join "\t", $chr, $snp, $rs{$chr}{$snp}, $gene{$chr}{$snp}, $ann{$chr}{$snp}, @line, "\n");
+#open (ASE, "test_5") || die "nope: $!";                                                                                                        
+    while (my $line = <ASE>) {
+	my @line = split " ", $line;
+	if ($line[0] =~ m/chr/ ) {
+	    my $chr = $line[0];
+	    my $snp = $line[1];
+	    print OUT ( join "\t", $chr, $snp, $rs{$chr}{$snp}, $gene{$chr}{$snp}, $ann{$chr}{$snp}, $line[2], $line[3], "\n");
+	}
+    }
+    close (OUT);
+    close (ASE);
 }
-close(ASE);
+close(FILES);
+
