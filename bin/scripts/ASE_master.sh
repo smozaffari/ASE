31fd7c65c33e8d491a8d392fb5a44cae1416e8ac
Sahar Mozaffari <smozaffari@uchicago.edu>
2016-05-02 10:06:45 -0500

    ASE

--- bin/scripts/ASE_master.sh
+++ bin/scripts/ASE_master.sh
@@ -5,7 +5,7 @@ setup_log=${scriptName}_${LOGNAME}_${timeTag}.log
 echo $setup_log
 echo "RUNNING $scriptName as " $(readlink -f $0 ) " on " `date`  | tee  $setup_log
 
-cat 989_ASE_info | while read i; do
+cat remainder_470_ASE_info | while read i; do
     echo $i
     qsub -v FILE=$i -N $i  /lustre/beagle2/ober/users/smozaffari/ASE/bin/scripts/ASE.pbs 2>&1
     echo -e "qsub -v $FILE=\"$i\" -N \"$i\" /lustre/beagle2/ober/users/smozaffari/ASE/bin/scripts/ASE.pbs"
