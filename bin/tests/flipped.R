b8d8f8fcf8b615c98979a3e804b2bf0305792286
smozaffari <smozaffari@uchicago.edu>
2016-05-30 23:28:59 -0500

    working

--- bin/tests/flipped.R
+++ bin/tests/flipped.R
@@ -1,3 +1,6 @@
+pvals <- c()
+names(pvals) <- c()
+
 maternal <- read.table("/group/ober-resources/users/smozaffari/ASE/data/expression/Maternal_gene_normalized.txt", check.names = F)
 paternal <- read.table("/group/ober-resources/users/smozaffari/ASE/data/expression/Paternal_gene_normalized.txt", check.names = F)
 genes <- rownames(maternal)
@@ -8,13 +11,38 @@ total <- read.table("/group/ober-resources/users/smozaffari/ASE/data/expression/
 maternal2 <- as.matrix(maternal)
 paternal2 <- as.matrix(paternal)
 
-#permute <- function() {
-  
-#}
+tstat <- function(tab) {
+      s <- split(tab, tab$V3)
+      het1p <- mean(na.omit(as.numeric(as.character(unlist(s[[1]][1])))))
+      het2p <- mean(na.omit(as.numeric(as.character(unlist(s[[2]][1])))))
+      het1m <- mean(na.omit(as.numeric(as.character(unlist(s[[1]][2])))))
+      het2m <- mean(na.omit(as.numeric(as.character(unlist(s[[2]][2])))))
+
+      T = (het1p-het2p)^2+(het1m-het2m)^2
+      list(h1p=het1p, h2p=het2p, h1m=het1m, h2m=het2m, T=T)
+}
+
+sig <- function(vec, newstat) {
+    pval <-  length(which(vec<newstat))/(length(vec)+1)
+    p2<- sprintf("%.10f",pval)
+    return(p2)
+}
+
+permute <- function(tab, num) {
+	vec <- NULL
+	orig <- tstat(tab)
+	true <- orig$T	
+	for (n in 1:num) {
+		tab1 <- tab[, c(sample(c(1,2)),3)]
+		tab2 <- tab1[sample(nrow(tab1)),]
+	    	vec <- c(vec, tstat(tab2)$T)
+	}
+	list(vals=vec)
+	sig(vec, true)	   
+}
 
 
 #for (i in 1:dim(maternal2)[1]) {
-#for (i in 7:7) {
 i <- 7
   print( genes[i])
   command <- paste("grep -w ",genes[i], " /group/ober-resources/users/smozaffari/ASE/data/ensemble_table_hg19_05.31  | cut -f2-5,12 | uniq", sep="")
@@ -30,45 +58,63 @@ i <- 7
   for (m in 1:length(mins)) {
       c <- substr(chr[m], 4, nchar(as.character(chr[m])))
       print(c);  
-#    system(paste("plink --bfile /group/ober-resources/users/smozaffari/ASE/data/plinkfiles/Hutterite_paternal --chr ",
-#              c ," --from-bp ",
-#              mins[m], " --to-bp ", maxs[m],
-#              " --recode --out /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snppat", names(total)[m] , sep=""))
-#    system(paste("plink --bfile /group/ober-resources/users/smozaffari/ASE/data/plinkfiles/Hutterite_maternal --chr ",
-#              c ," --from-bp ",
-#              mins[m], " --to-bp ", maxs[m],
-#              " --recode --out /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m], sep=""))
-#	      }
-    if (file.exists(paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m], ".map", sep=""))) {
-       pat <- read.table(paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snppat", names(total)[m] ".ped" , sep=""))
-       mat <- read.table(paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m] ".ped" , sep=""))
-       pat[1:5,1:5]
+    system(paste("plink --bfile /group/ober-resources/users/smozaffari/ASE/data/plinkfiles/Hutterite_paternal --chr ",
+              c ," --from-bp ",
+              mins[m], " --to-bp ", maxs[m],
+              " --recode --out /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snppat", names(total)[m] , sep=""))
+    system(paste("plink --bfile /group/ober-resources/users/smozaffari/ASE/data/plinkfiles/Hutterite_maternal --chr ",
+              c ," --from-bp ",
+              mins[m], " --to-bp ", maxs[m],
+              " --recode --out /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m], sep=""))
+     file <- paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m], ".map", sep="")
+     print(file)
+    if (file.exists(file)) {
+       pat <- read.table(paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snppat", names(total)[m], ".ped" , sep=""))
+       mat <- read.table(paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m], ".ped" , sep=""))
+       print(pat[1:5,1:5])
        Findiv <- substr(as.numeric(mat$V2), 1, nchar(mat$V2)-1)
+       print(head(Findiv))
        genos <- (dim(mat)[2]-6)/2
-       print genos
+       print(genos)
        for (g in 1:genos) {
        	   col <- g+7
-       	   gtype <- cbind(Findiv, as.character(pat[,col]), as.character(mat[,col]))
+	   print(col)
+	   print(head(pat[[col]]))
+       	   gtype <- cbind(Findiv, as.character(pat[[col]]), as.character(mat[[col]]))
+	   print(head(gtype))
        	   colnames(gtype) <- c("Findiv", "Pat", "Mat")
-	   removedgt <- which(!Findiv%in%colnames(maternal))
-	   gtype2 <- as.data.frame(gtype[-c(removedgt),])
-	   gtype3 <- gtype2[match(colnames(maternal), as.character(gtype2$Findiv)),]
+	   findiv <- sort(Findiv)
+	   lostgt <- which(Findiv%in%colnames(maternal))
+	   gtype2 <- as.data.frame(gtype[c(lostgt),])
+	   print(head(gtype2))
+	   maternal424 <- maternal[,c(which(colnames(maternal)%in%Findiv))]
+	   paternal424 <- paternal[,c(which(colnames(paternal)%in%Findiv))]
+	   gtype3 <- gtype2[match(colnames(maternal424), as.character(gtype2$Findiv)),]
 	   gtype3$GG <- paste(gtype3$Pat, gtype3$Mat, sep=":")
-	   head(gtype3)
-	}
+	   print(head(gtype3))
+	   tgg <- table(gtype3$GG)
+	   print(tgg)
+	   hets <- which(!gtype3$Pat==gtype3$Mat)
+	   new <- as.data.frame(cbind(unlist(c(maternal424[i,hets])), unlist(c(paternal424[i,hets])),unlist(c(gtype3[hets,"GG"]))))
 
-        #lcommand <- (paste("wc -l /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m],".map", sep=""))
-	#lines <- read.table(text=system(command), intern=TRUE)
-    	#print (lines)
-	#str(lines)
-	#print (as.string(as.character(lines)))
-    	#num <- strsplit(as.character(lines), " ")
-	#print (num[1])
+	   nn <- i+m-1
+	   pvals[nn] <- permute(new, 1000)
+#	   pvals <- c(pvals,  permute(new, 1000))
+	   print(pvals)
+	   print(names(pvals))
+	   print(names(total[m]))
+	   names(pvals)[nn] <- names(total)[m])
+	   print(pvals)
+	   write.table(pvals, "pvalues", row.names = F, quote = F)
+
+	}
+	system(paste("rm /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snppat", names(total)[m], ".*" , sep=""))
+
 }
 }
+}
+
+
+write.table(pvals, "pvalues", row.names = F, quote = F)
 
 	      
-#mc <- mean(maternal2[i,])
-#  pc <- mean(paternal2[i,])
-#  T = 
-#}
