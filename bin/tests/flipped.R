894a1509a8155a472e5ed8e60421e5769bfe8718
smozaffari <smozaffari@uchicago.edu>
2016-05-31 14:40:36 -0500

    working! on more than 3 people...

--- bin/tests/flipped.R
+++ bin/tests/flipped.R
@@ -4,6 +4,9 @@ totalsnps <- c()
 totalpeople <- c()
 mm <- 0
 
+snpcount <- 1;
+genecount <- 0;
+
 maternal <- read.table("/group/ober-resources/users/smozaffari/ASE/data/expression/Maternal_gene_normalized.txt", check.names = F)
 paternal <- read.table("/group/ober-resources/users/smozaffari/ASE/data/expression/Paternal_gene_normalized.txt", check.names = F)
 genes <- rownames(maternal)
@@ -46,25 +49,33 @@ permute <- function(tab, num) {
 
 
 #for (i in 1:dim(maternal2)[1]) {
-for (i in 1:10) {
+for (i in 1:20) {
 #i <- 7
   print( genes[i])
+  genecount <- genecount+1
   command <- paste("grep -w ",genes[i], " /group/ober-resources/users/smozaffari/ASE/data/ensemble_table_hg19_05.31  | cut -f2-5,12 | uniq", sep="")
   snps <- try(read.table(text=system(command, intern=TRUE)))
   if (class(snps) =='try-error') {
      snps=NULL
-     pvals[mm+1] <- "NA"
-     names(pvals)[mm+1] <- genes[i]
-     totalsnps[mm+1] <- "NA"
-     names(totalsnps)[mm+1] <- genes[i]
+#     mm <- snpcount+genecount
+#     pvals[mm+1] <- "NA"
+     pvals[snpcount] <- "NA"
+     names(pvals)[snpcount] <- genes[i]
+     totalsnps[genecount] <- "NA"
+     names(totalsnps)[genecount] <- genes[i]
+     totalpeople[snpcount] <- "NA"
+     names(totalpeople)[snpcount] <- genes[i]
+     snpcount <- snpcount+1
   }
   if (!is.null(snps)) {
   total <- split( snps , f = snps$V5 )
   mins <- sapply( total , function(x) min( x$V4 )+250000 )
   maxs <- sapply( total , function(x) max( x$V4 )+250000 )
   chr <- sapply( total , function(x) unique(x$V1) )
-  for (m in 1:length(mins)) {
-      nn <- i+m-1
+  totmatch <- length(total)
+  for (m in 1:totmatch) {
+      genecount <- genecount+m-1
+#      nn <- i+m-1
       c <- substr(chr[m], 4, nchar(as.character(chr[m])))
       print(c);  
     system(paste("plink --bfile /group/ober-resources/users/smozaffari/ASE/data/plinkfiles/Hutterite_paternal --chr ",
@@ -82,8 +93,8 @@ for (i in 1:10) {
        map <- read.table(paste("/group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snpmat", names(total)[m], ".map" , sep=""))
        Findiv <- substr(as.numeric(mat$V2), 1, nchar(mat$V2)-1)
        genos <- (dim(mat)[2]-6)/2
-       totalsnps[nn] <-  genos
-       names(totalsnps)[nn] <- names(total)[m]
+       totalsnps[genecount] <-  genos
+       names(totalsnps)[genecount] <- names(total)[m]
        for (g in 1:genos) {
        	   col <- g+7
        	   gtype <- cbind(Findiv, as.character(pat[[col]]), as.character(mat[[col]]))
@@ -93,16 +104,18 @@ for (i in 1:10) {
 	   gtype2 <- as.data.frame(gtype[c(lostgt),])
 	   maternal424 <- maternal[,c(which(colnames(maternal)%in%Findiv))]
 	   paternal424 <- paternal[,c(which(colnames(paternal)%in%Findiv))]
-	   mm <- i+m+g-1
-	   totalpeople[mm] <- length(which(paternal424[i,]>0))
-	   names(totalpeople)[mm] <- (paste(names(total)[m], map$V1[g], map$V4[g], sep="_"))
-	   if (totalpeople[mm] > 10) {
-	      print(totalpeople[mm])
-	   gtype3 <- gtype2[match(colnames(maternal424), as.character(gtype2$Findiv)),]
+#	   mm <- i+m+g-1
+	   lots <- length(which(paternal424[i,]>0))
+	   str(lots)
+	   print(lots)
+	   totalpeople[snpcount] <- length(which(paternal424[i,]>0))
+	   names(totalpeople)[snpcount] <- (paste(names(total)[m], map$V1[g], map$V4[g], sep="_"))
+
+	   if ( totalpeople[snpcount] >= 10) {
+	      print(totalpeople[snpcount])
+	      	   gtype3 <- gtype2[match(colnames(maternal424), as.character(gtype2$Findiv)),]
 	   gtype3$GG <- paste(gtype3$Pat, gtype3$Mat, sep=":")
-#	   print(head(gtype3))
 	   tgg <- table(gtype3$GG)
-	   print(tgg)
 	   if (length(levels(gtype3$Mat)< 3)) {
 	      if (length(levels(gtype3$Pat) ==3 )) {
 	   	   gtype3$Mat <- factor(gtype3$Mat, levels=levels(gtype3$Pat))
@@ -119,22 +132,25 @@ for (i in 1:10) {
 	   
 	   	   new <- as.data.frame(cbind(unlist(c(maternal424[i,hets])), unlist(c(paternal424[i,hets])),unlist(c(gtype3[hets,"GG"]))))
 		   mm <- i+m+g-1
-	   	   pvals[mm] <- permute(new, 1000)
-	#		  print(pvals)
-#		print(names(pvals))
-#		print(names(total[m]))
-		names(pvals)[mm] <- (paste(names(total)[m], map$V1[g], map$V4[g], sep="_"))
-#		print(pvals)
+	   	   pvals[snpcount] <- permute(new, 1000)
+		names(pvals)[snpcount] <- (paste(names(total)[m], map$V1[g], map$V4[g], sep="_"))
+	    } else {
+          pvals[snpcount] <- 'NA'
+          names(pvals)[snpcount] <- (paste(names(total)[m], map$V1[g], map$V4[g], sep="_"))
 	    }
 	   write.table(t(pvals), "pvalues.txt", row.names = F, quote = F)
 	   }
 	}
+	snpcount <- snpcount+1
 	}
 	} else {
-	  totalsnps[mm+1] <- 0
-	  names(totalsnps)[mm+1] <- names(total)[m]
-	  totalpeople[mm+1] <- 0
-	  names(totalpeople)[mm+1] <- names(total)[m]
+	  totalsnps[genecount] <- 0
+	  names(totalsnps)[genecount] <- names(total)[m]
+	  totalpeople[snpcount] <- 0
+	  names(totalpeople)[snpcount] <- names(total)[m]
+	  pvals[snpcount] <- 'NA'
+	  names(pvals)[snpcount] <- names(total)[m]	  
+	  snpcount <- snpcount+1
 	 }
 	system(paste("rm /group/ober-resources/users/smozaffari/ASE/data/tests_flipped/", chr[m], "_snppat", names(total)[m], ".*" , sep=""))
 
@@ -143,7 +159,10 @@ for (i in 1:10) {
 }
 
 
-write.table(t(pvals), "pvalues.txt", quote = F)
-write.table(t(totalsnps), "totalsnps.txt", quote=F )
-write.table(t(totalpeople), "totalpeople.txt", quote=F )
+pvals2 <- cbind(names(pvals), pvals)
+write.table(pvals2, "pvalues.txt", quote = F, row.names = F)
+tot2 <- cbind(names(totalsnps), totalsnps)
+write.table(tot2, "totalsnps.txt", quote=F, row.names = F )
+pp2 <- cbind(names(totalpeople), totalpeople)
+write.table(pp2, "totalpeople.txt", quote=F, row.names = F )
 
