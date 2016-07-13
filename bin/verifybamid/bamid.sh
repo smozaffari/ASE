00629e7a82f0398dd587d6ac39e8da1286b67cee
Sahar Mozaffari <smozaffari@uchicago.edu>
2016-07-13 13:30:42 -0500

    original

--- bin/verifybamid/bamid.sh
+++ bin/verifybamid/bamid.sh
@@ -2,7 +2,7 @@
 #PBS -l mppwidth=30
 #PBS -l walltime=02:00:00
 #PBS -j oe 
-#PBS -N bamid
+#PBS -N bamid_$FI
 ## Beagle specific
 #PBS -A CI-MCB000155
 
@@ -27,8 +27,10 @@ echo $setup_log
 echo "RUNNING $scriptName as " $(readlink -f $0 ) " on " `date`  | tee  $setup_log
 
 
+aprun -n 1 -N 1 -d 15 plink-1.9 
 
-aprun -n 1  -N 1  -d 30 /lustre/beagle2/ober/users/smozaffari/verifyBamID/verifyBamID/bin/verifyBamID --vcf Hutterite_sexcheck_kao.vcf --bam /lustre/beagle2/ober/users/smozaffari/ASE/results/star/FlowCell7/160591/160591_lane_7.sorted.sort.bam --ignoreRG --smID HUTTERITES_106592 --verbose --out test_106592 
+aprun -n 1  -N 1  -d 15  /lustre/beagle2/ober/users/smozaffari/verifyBamID/verifyBamID/bin/verifyBamID --vcf Hutterite_sexcheck_kao_correct.vcf --bam /lustre/beagle2/ober/users/smozaffari/ASE/results/star/FlowCell7/160591/160591_lane_7.sorted.bam  --ignoreRG --smID HUTTERITES_160591 --self --verbose --out test_160591_exp160591
+
 
 
 
