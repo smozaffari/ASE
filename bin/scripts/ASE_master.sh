b4769cb2b345b896b367c56390e01c3a7a4025ae
Sahar <smozaffari@uchicago.edu>
2016-09-20 15:05:24 -0500

    Update ASE_master.sh

--- bin/scripts/ASE_master.sh
+++ bin/scripts/ASE_master.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Author: SVM 
+# Date: 09/20/16
+# Purpose: put together ASE for all of the files. 
+# reads from 989_star_overhang_ASE_info - but this file may be too big (submit too many jobs here - consider splitting into two)
 
 setup_log=${scriptName}_${LOGNAME}_${timeTag}.log
 echo $setup_log
 echo "RUNNING $scriptName as " $(readlink -f $0 ) " on " `date`  | tee  $setup_log
 
-cat queud | while read i; do
+cat 989_star_overhang_ASE_info    | while read i; do
     echo $i
     qsub -v FILE=$i -N $i  /lustre/beagle2/ober/users/smozaffari/ASE/bin/scripts/ASE.pbs 2>&1
     echo -e "qsub -v $FILE=\"$i\" -N \"$i\" /lustre/beagle2/ober/users/smozaffari/ASE/bin/scripts/ASE.pbs"
