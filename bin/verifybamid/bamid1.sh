a8ef65e997cd13e4c4a955fdffd50db1df58d993
Sahar <smozaffari@uchicago.edu>
2017-01-24 15:08:01 -0600

    added notes

--- bin/verifybamid/bamid1.sh
+++ bin/verifybamid/bamid1.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-# Author: SVM 
-\# Purpose: loop through RNAseq data and check if genotypes match individual
+# Author: Sahar
+# 8/23/2016
+# Beagle
 
+# PURPOSE: loop through RNAseq data and check if genotypes match individual
+# INPUT: file with how RNAseq folders are arranged, including Flowcell and Findiv as a folder and subfolder
+# 	 swaps.txt --> lustre/beagle2/ober/users/smozaffari/ASE/results/swaps_genotype/swaps.txt
+# 	 file made from rna seq data results folder - want to do each flowcell/lane/person individually 
+# 	 these are how my RNA seq files are arranged, by FlowCell/FINDIV/lane (how Darren gave them to me)
+
+# 	 input file ex. 
+# 	 FlowCell11.108211.lane_3
+# 	 FlowCell11.108211.lane_4
+# 	 FlowCell9.108211.lane_5
+# 	 FlowCell9.108211.lane_4
 
 inputFile=$(readlink -f $1)
 
+
+# this will change depending on how many people you are checking.
 jobsPerNode=2
 NNodes=1
 NCoresPerNode=2
@@ -35,7 +49,7 @@ FClist=""
 while read -r line;do
     echo $line;
 
-
+# extract FINDIV and add to FINDIV list to qsub multiple together
     FINDIV=$(echo $line | cut -f2 -d".")
     echo $FINDIV
 
@@ -44,9 +58,8 @@ while read -r line;do
     else 
 	FINDIVlist=$FINDIV
     fi
-
-
-
+    
+# extract Flowcell and add to list to qsub multiple together
     FC=$(echo $line | cut -f1 -d".")
     if [ "$FClist" ]; then
 	FClist="${FClist}:${FC}"
@@ -54,8 +67,8 @@ while read -r line;do
 	FClist=$FC
     fi
 
+# same thing for lane
     lane=$(echo $line | cut -f3 -d".")
-
     if [ "$lanelist" ]; then
 	lanelist="${lanelist}:${lane}"
     else
@@ -65,6 +78,8 @@ while read -r line;do
     count=$(($count+1))
     nTotSubJobs=$(($nTotSubJobs+1))
 
+# once the number it has read through equals the number of files you wanted to run per node, it will qsub the next one
+# also empty the lists to continue
     if [ "$count" -eq "$filesPerNode" ] || [ "$nTotSubJobs" -eq "$NInputFiles" ]; then
 	echo $count $nTotSubJobs
 	echo -e "qsub -v JOBSPERNODE=$jobsPerNode,FINDIV=$FINDIVlist,FC=$FClist,LANE=$lanelist,COUNT=$nTotSubJobs -N swap1_${nTotSubJobs} $scriptDir/bamid.pbs" | tee -a $setup_log
