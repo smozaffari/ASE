a0d42fc05f142be58f6066b94a9ccfccd4022730
Sahar Mozaffari <smozaffari@uchicago.edu>
2017-08-23 13:12:24 -0500

    annotate reads

--- bin/scripts/gene.pl
+++ bin/scripts/gene.pl
@@ -7,33 +7,37 @@ my %gene;
 my %rs;
 my %ann;
 
-open (ANN, "/lustre/beagle2/ober/users/smozaffari/ASE/data/all12_imputed_cgi.annovar_plink_annotations.hg19_multianno.txt") || die "nope: $!";
+open (ANN, "/lustre/beagle2/ober/users/smozaffari/vepannotation_genes") || die "nope: $!";
 my $f = <ANN>;
 while (my $line = <ANN>) {
     my @line = split "\t", $line;
-    my $chr = $line[0];
     my $loc = $line[1];
-    $rs{$chr}{$loc} = $line[14];
-    $gene{$chr}{$loc} = $line[6];
-    $ann{$chr}{$loc} = $line[5];
+    $rs{$loc} = $line[12];
+    $gene{$loc} =$line[14];
+    chomp $gene{$loc};
+    $ann{$loc} = $line[6];
 }
 
-open (FILES, "/lustre/beagle2/ober/users/smozaffari/ASE/results/test_ASE") || die "nope: $!";
+open (FILES, "/lustre/beagle2/ober/users/smozaffari/ASE/results/989_ASE_info_v19") || die "nope: $!";
 while (my $fileline = <FILES>) {
     my @findiv_lane = split "/", $fileline;
     my $name = $findiv_lane[3];
+    my $fc = $findiv_lane[1];
     chomp $name;
-    my $outfile = "out_$name.txt";
-    open (OUT, ">summarystats/$outfile") || die "nope: $!";
+    my $outfile = "out_${fc}_${name}.txt";
+    open (OUT, ">summarystats_v19/$outfile") || die "nope: $!";
     open (ASE, $fileline) || die "nope: $!";
 #open (ASE, "/lustre/beagle2/ober/users/smozafari/ASE/results/withoutsaved/FlowCell8/122462/122462_lane_5_ASE_info") || die "nope: $!";
 #open (ASE, "test_5") || die "nope: $!";                                                                                                        
     while (my $line = <ASE>) {
 	my @line = split " ", $line;
-	if ($line[0] =~ m/chr/ ) {
-	    my $chr = $line[0];
-	    my $snp = $line[1];
-	    print OUT ( join "\t", $chr, $snp, $rs{$chr}{$snp}, $gene{$chr}{$snp}, $ann{$chr}{$snp}, $line[2], $line[3], "\n");
+	if ($line[1] =~ m/chr/ ) {
+	    my $chr = $line[1];
+	    $chr =~ s/\D//g;
+	    my $snp = $line[3];
+	    my $newsnp = join (":", $chr, $snp);
+#	    print $newsnp;
+	    print OUT ( join "\t", $fc, $name, $chr, $snp, $rs{$newsnp}, $gene{$newsnp}, $ann{$newsnp}, $line[4], $line[5], $line[7], "\n");
 	}
     }
     close (OUT);
