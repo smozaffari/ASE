ba70fe0ef00c8c3020297e0863413e1a525f4665
smozaffari <smozaffari@uchicago.edu>
2018-03-16 16:20:38 -0500

    het SNPs

--- bin/tarbellscripts/po_genotypes.sh
+++ bin/tarbellscripts/po_genotypes.sh
@@ -22,4 +22,4 @@ while read LINE; do
     echo "qsub -v FINDIV=$LINE $scriptDir/geno.pbs"
     qsub -v FINDIV=$LINE $scriptDir/geno.pbs
     sleep 3
-done < /group/ober-resources/users/smozaffari/ASE/data/allwithRNAseq
+done < /group/ober-resources/users/smozaffari/ASE/data/justfindivs
