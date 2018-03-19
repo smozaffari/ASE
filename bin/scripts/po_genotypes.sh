10077bf1a8cebcee9199f875d0e2e38fdc7d0c28
Sahar Mozaffari <smozaffari@uchicago.edu>
2018-03-19 11:14:53 -0500

    po genotypes for heterozygous sites only

---
+++ bin/scripts/po_genotypes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Script that will be executed on the compute nodes
+
+. /opt/modules/default/init/bash
+if [ ! $(module list -t 2>&1 | grep PrgEnv-gnu) ]; then
+ module swap PrgEnv-cray PrgEnv-gnu
+fi
+
+module load python/2.7.6-vanilla 
+
+FINDIV=$2
+SCRIPTDIR=$1
+
+cd $PBS_O_WORKDIR
+echo $PBS_O_WORKDIR
+
+cd $FINDIV
+echo $PBS_O_WORKDIR/$FINDIV
+
+echo "FINDIV: " $FINDIV
+
+echo "plink-1.9 --bfile /lustre/beagle2/ober/users/smozaffari/POeQTL/results/gexp/paternal_imputed_gexp --keep ${FINDIV}1.txt --geno 0 --recode transpose --out ${FINDIV}1"
+plink-1.9 --bfile /lustre/beagle2/ober/users/smozaffari/POeQTL/results/gexp/paternal_imputed_gexp --keep ${FINDIV}1.txt  --geno 0 --recode transpose --out ${FINDIV}1
+echo "plink-1.9 --bfile /lustre/beagle2/ober/users/smozaffari/POeQTL/results/gexp/maternal_imputed_gexp --keep ${FINDIV}2.txt --geno 0 --recode transpose --out ${FINDIV}2"
+plink-1.9 --bfile /lustre/beagle2/ober/users/smozaffari/POeQTL/results/gexp/maternal_imputed_gexp --keep ${FINDIV}2.txt --geno 0 --recode transpose --out ${FINDIV}2
+
+awk 'NR==FNR{a[$2]==$0;next;} $2 in a{print $0}' ${FINDIV}1.tped ${FINDIV}2.tped > ${FINDIV}2.tped2
+awk 'NR==FNR{a[$2]==$0;next;} $2 in a{print $0}' ${FINDIV}2.tped ${FINDIV}1.tped > ${FINDIV}1.tped2
+
+
+paste <(awk '{print "chr" $1 " " $4 " " $5}' ${FINDIV}1.tped2) <(awk '{print $5}' ${FINDIV}2.tped2) | sed 's/\t/ /g' > ${FINDIV}_SNPs
+
+awk '{if ($3!=$4) print $0}' ${FINDIV}_SNPs > ${FINDIV}_het_SNPs
+
+rm *.tped*
+rm *.txt
+rm *.tfam
+rm *.log
+for i in `seq 1 22`
+do
+   awk -v chr="chr$i" '$1 == chr {print}' ${FINDIV}_het_SNPs > chr${i}.het.snps.txt
+   wait
+   gzip chr${i}.het.snps.txt
+   rm chr${i}.het.snps.txt
+done
---
+++ bin/scripts/po_genotypes_3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Author: SVM 
+# Purpose: make PO genotype files for hets
+# USAGE: bash po_genotypes_3.sh 
+
+#jobsPerNode=$2 #How many jobs per node = 1..32 what parallel gets 1-32 specific to Beagle
+#NNodes=$3 #Number of nodes you want to use /available (for 1000 files, and about 20 jobs/node only need 50 nodes)
+
+
+scriptName=$(basename ${0})
+scriptName=${scriptName%\.sh}
+scriptDir=$(readlink -f "$(dirname "$0")")
+echo $scriptDir
+
+timeTag=$(date "+%y_%m_%d_%H_%M_%S")
+
+setup_log=${scriptName}_${LOGNAME}_${timeTag}.log
+echo $setup_log
+echo "RUNNING $scriptName as " $(readlink -f $0 ) " on " `date`  | tee  $setup_log
+
+while read LINE; do
+    echo "$LINE"
+    mkdir $LINE
+    echo $LINE
+    echo "HUTTERITES ${LINE}2" > $LINE/${LINE}2.txt
+    echo "HUTTERITES ${LINE}1" > $LINE/${LINE}1.txt
+    echo "qsub -v FINDIV=$LINE SCRIPTDIR=$scriptDir -N $LINE $scriptDir/po_genotypes_2.pbs" | tee -a $setup_log
+    qsub -v FINDIV=$LINE,SCRIPTDIR=$scriptDir -N $LINE $scriptDir/po_genotypes_2.pbs 2>&1
+    sleep 3
+done < /lustre/beagle2/ober/users/smozaffari/POeQTL/results/gexp/justfindivs
+
+
+
