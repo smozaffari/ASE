e43fd4a4cf28aa4c6d17c6b3050ed1b82fbfba53
smozaffari <smozaffari@uchicago.edu>
2018-06-11 15:42:54 -0500

    seem old -4-6/2017

---
+++ bin/SNPfiles/po_genotypes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+
+scriptName=$(basename ${0})
+scriptName=${scriptName%\.sh}
+scriptDir=$(readlink -f "$(dirname "$0")")
+echo $scriptDir
+
+timeTag=$(date "+%y_%m_%d_%H_%M_%S")
+
+setup_log=${scriptName}_${LOGNAME}_${timeTag}.log
+echo $setup_log
+echo "RUNNING $scriptName as " $(readlink -f $0 ) " on " `date`  | tee  $setup_log
+
+
+while read LINE; do
+    echo "$LINE"
+    mkdir $LINE
+    echo $LINE
+    echo "HUTTERITES ${LINE}2" > $LINE/${LINE}2.txt
+    echo "HUTTERITES ${LINE}1" > $LINE/${LINE}1.txt
+    echo "HUTTERITES ${LINE}2" > $LINE/${LINE}.txt
+    echo "HUTTERITES ${LINE}1" >> $LINE/${LINE}.txt
+    echo "qsub -v FINDIV=$LINE $scriptDir/geno.pbs"
+    qsub -v FINDIV=$LINE $scriptDir/geno.pbs
+    sleep 3
+done < uniq_withPO_version2
